0eee6f384208c1b0942a04f74451397f2a170f0f
Fu-Yilei <fuyilei96@outlook.com>
2019-03-26 14:08:48 +0800

    bug fix

--- train/msaprobs/refdivideandscore.py
+++ train/msaprobs/refdivideandscore.py
@@ -51,6 +51,6 @@ for f in rpfilelist:
     with open(rpout+f, 'w') as file:
         for i in range(len(original_protein_n)):
             name = original_protein_n[i] + '\n'
-            line = divided[1][i]'\n'
+            line = divided[1][i] + '\n'
             file.write(name)
             file.write(line)
