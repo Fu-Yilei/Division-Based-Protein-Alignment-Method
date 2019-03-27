08404180ab2bee51811c342897c16f376c18875f
Fu-Yilei <fuyilei96@outlook.com>
2019-03-27 20:24:10 +0800

    another fraction

--- train/msaprobs/train.py
+++ train/msaprobs/train.py
@@ -16,7 +16,7 @@ import os
 ff = "0.3"
 sf = "0.7"
 #delete remain files
-os.system("rm -r /data3/fuyilei96/ProteinTest/msaprobs/sabre /data3/fuyilei96/ProteinTest/msaprobs/ox /data3/fuyilei96/ProteinTest/msaprobs/bali3")
+# os.system("rm -r /data3/fuyilei96/ProteinTest/msaprobs/sabre /data3/fuyilei96/ProteinTest/msaprobs/ox /data3/fuyilei96/ProteinTest/msaprobs/bali3")
 #mkdir
 os.system("mkdir  /data3/fuyilei96/ProteinTest/msaprobs/sabre /data3/fuyilei96/ProteinTest/msaprobs/ox /data3/fuyilei96/ProteinTest/msaprobs/bali3")
 os.system("mkdir /data3/fuyilei96/ProteinTest/msaprobs/sabre/" + "_ff_" + ff +"_sf_" + sf + "/" )
