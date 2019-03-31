bae2c90311357e37d1377f00429fb225f32e73bb
Fu-Yilei <fuyilei96@outlook.com>
2019-03-31 13:54:47 +0800

    new fraction

--- train/msaprobs/train.py
+++ train/msaprobs/train.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-''' 
+'''
  * All rights Reserved, Designed By HIT-Bioinformatics   
  * @Title:  run.py
  * @Description: Run program
@@ -12,8 +12,8 @@
 
 import sys
 import os
-for i in range(15, 45, 5):
-    for j in range(55, 95, 5):
+for i in range(20, 30, 5):
+    for j in range(60, 80, 5):
         ff = str(i/100.0)
         sf = str(j/100.0)
         #delete remain files
@@ -30,11 +30,11 @@ for i in range(15, 45, 5):
         os.system("python divide.py -orf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/bali3/in/ -ouf /data3/fuyilei96/ProteinTest/msaprobs/bali3/" + "_ff_" + ff +"_sf_" + sf + "/" + " -ff " + ff + " -sf " + sf)
 
         #Calculating Scores
-        scorepath = "/data3/fuyilei96/Division-Based-Protein-Alignment-Method/train/msaprobs/scores_ff_" + ff +"_sf_" + sf + "/"
+        scorepath = "/data3/fuyilei96/ProteinTest/msaprobs/scores_ff_" + ff +"_sf_" + sf + "/"
         os.system("mkdir " + scorepath)
-        os.system("python score.py -af /data3/fuyilei96/ProteinTest/msaprobs/sabre/" + "_ff_" + ff +"_sf_" + sf + "/output/ -rf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/sabre/ref/ -op " + scorepath + " -dbn sabre")
-        os.system("python score.py -af /data3/fuyilei96/ProteinTest/msaprobs/ox/" + "_ff_" + ff +"_sf_" + sf + "/output/ -rf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/ox/ref/ -op " + scorepath + " -dbn ox")
-        os.system("python score.py -af /data3/fuyilei96/ProteinTest/msaprobs/bali3/" + "_ff_" + ff +"_sf_" + sf + "/output/ -rf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/bali3/ref/ -op " + scorepath + " -dbn bali3")
+        os.system("python score_8.py -af /data3/fuyilei96/ProteinTest/msaprobs/sabre/" + "_ff_" + ff +"_sf_" + sf + "/output/ -rf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/sabre/ref/ -op " + scorepath + " -dbn sabre")
+        os.system("python score_8.py -af /data3/fuyilei96/ProteinTest/msaprobs/ox/" + "_ff_" + ff +"_sf_" + sf + "/output/ -rf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/ox/ref/ -op " + scorepath + " -dbn ox")
+        os.system("python score_8.py -af /data3/fuyilei96/ProteinTest/msaprobs/bali3/" + "_ff_" + ff +"_sf_" + sf + "/output/ -rf /home/fuyilei96/ProteinAlignment/proteinalignment/benchmark/bali3/ref/ -op " + scorepath + " -dbn bali3")
 
             
 # #Calculating Original Scores
